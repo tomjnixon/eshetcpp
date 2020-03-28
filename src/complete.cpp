ec7fb7166dc3a065ecc57947e72aceec0e703d5b
Thomas Nixon <tom@tomn.co.uk>
2020-03-28 13:06:37 +0000

    fix completion

--- src/complete.cpp
+++ src/complete.cpp
@@ -16,8 +16,7 @@ std::pair<std::string, std::string> split_path(std::string path) {
 }
 
 int main(int argc, char **argv) {
-  ESHETClient client(get_host_port());
-  client.wait_connected().get();
+  ActorThread<ESHETClient> client(get_host_port());
 
   assert(argc >= 3);
   std::string word = argv[2];
@@ -25,26 +24,36 @@ int main(int argc, char **argv) {
   std::string dir_part, prefix;
   std::tie(dir_part, prefix) = split_path(word);
 
-  auto res = client.action_call_promise("/meta/ls", dir_part).get();
-  auto res_mp = res.value.get();
+  Channel<Result> call_result;
+  client.action_call_pack("/meta/ls", call_result, std::make_tuple(dir_part));
+  auto res = call_result.read();
 
-  if (res_mp.type == msgpack::type::object_type::ARRAY) {
-    assert(res_mp.via.array.size == 2);
-    assert(res_mp.via.array.ptr[0].as<std::string>() == "dir");
+  client.exit();
 
-    auto entries = res_mp.via.array.ptr[1]
-                       .as<std::vector<std::pair<std::string, std::string>>>();
+  if (!std::holds_alternative<Success>(res))
+    return 0;
 
-    for (auto &entry : entries) {
-      std::string name, type;
-      std::tie(name, type) = entry;
+  auto res_mp = std::get<Success>(res).value.get();
 
-      if (name.substr(0, prefix.size()) == prefix) {
-        if (type == "dir")
-          std::cout << dir_part << name << "/" << std::endl;
-        else
-          std::cout << dir_part << name << " " << std::endl;
-      }
+  if (res_mp.type != msgpack::type::object_type::ARRAY)
+    return 1;
+
+  // expect ["dir", entries], where each entry is [type, name]
+  assert(res_mp.via.array.size == 2);
+  assert(res_mp.via.array.ptr[0].as<std::string>() == "dir");
+
+  auto entries = res_mp.via.array.ptr[1]
+                     .as<std::vector<std::pair<std::string, std::string>>>();
+
+  for (auto &entry : entries) {
+    std::string name, type;
+    std::tie(name, type) = entry;
+
+    if (name.substr(0, prefix.size()) == prefix) {
+      if (type == "dir")
+        std::cout << dir_part << name << "/" << std::endl;
+      else
+        std::cout << dir_part << name << " " << std::endl;
     }
   }
 
