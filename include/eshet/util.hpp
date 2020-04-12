18d1b248998e81be3ca4d02d37c077cc7418af20
Thomas Nixon <tom@tomn.co.uk>
2020-04-12 12:55:42 +0100

    remove unused function

--- include/eshet/util.hpp
+++ include/eshet/util.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "data.hpp"
-#include <future>
 #include <type_traits>
 
 namespace eshet {
@@ -26,31 +25,6 @@ template <typename In, typename Cb> bool convert_variant(In in, Cb cb) {
   return std::visit(
       [&](auto x) { return detail::do_convert_variant(std::move(x), cb); },
       std::move(in));
-}
-
-// visitor to resolve a promise with a result; calls set_value for non-Error
-// inputs (which may well get converted to another variant), or set_exception
-// with Errors.
-template <typename PromiseT> struct ResultToPromise {
-  ResultToPromise(std::promise<PromiseT> &p) : p(p) {}
-  std::promise<PromiseT> &p;
-
-  void operator()(Success v) { p.set_value(std::move(v)); }
-  void operator()(Known v) { p.set_value(std::move(v)); }
-  void operator()(Unknown v) { p.set_value(std::move(v)); }
-  void operator()(Error v) {
-    p.set_exception(std::make_exception_ptr(std::move(v)));
-  }
-};
-
-template <typename PT, typename RT, typename CB>
-std::future<PT> wrap_promise(CB wrap_cb) {
-  auto p = std::make_shared<std::promise<PT>>();
-  auto cb = [p](RT r) { std::visit(ResultToPromise(*p), std::move(r)); };
-
-  wrap_cb(std::move(cb));
-
-  return p->get_future();
 }
 
 } // namespace detail
