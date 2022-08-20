9a8b439c2fc60fe24c70ea84160e4fc8d9cbe739
Thomas Nixon <tom@tomn.co.uk>
2022-08-20 18:59:50 +0100

    test state times

--- test/test_state.cpp
+++ test/test_state.cpp
@@ -89,3 +89,37 @@ TEST_CASE("test_reconnection") {
 
   check_connection(7);
 }
+
+TEST_CASE("test_time") {
+  using namespace std::chrono;
+  using clock = steady_clock;
+  using time_point = time_point<clock>;
+
+  ESHETClient client("localhost", 11236);
+
+  Actor self;
+  Channel<Result> result(self);
+  Channel<StateResult> state_result(self);
+  Channel<StateUpdate> on_change(self);
+
+  // register a state and change it
+  client.state_register(NS "/state3", result);
+  REQUIRE(std::holds_alternative<Success>(result.read()));
+
+  auto t_change = clock::now();
+  client.state_changed(NS "/state3", 5, result);
+  REQUIRE(std::holds_alternative<Success>(result.read()));
+
+  ESHETClient client2("localhost", 11236);
+
+  // after some time, observe it
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  auto t_observe = clock::now();
+  client2.state_observe(NS "/state3", state_result, on_change);
+  auto res = std::get<Known>(state_result.read());
+
+  // t_since_change should be about the time between changing and observing
+  auto diff = res.t_since_change - (t_observe - t_change);
+  auto diff_ms = duration_cast<milliseconds>(diff).count();
+  REQUIRE(std::abs(diff_ms) < 50);
+}
