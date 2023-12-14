c4fc81eab04b1307cb4014e2c45a07359adb525f
Thomas Nixon <tom@tomn.co.uk>
2023-12-14 00:54:07 +0000

    fixup comment

--- src/utils.hpp
+++ src/utils.hpp
@@ -3,6 +3,6 @@
 #include <utility>
 
 /// get the ESHET host and port from the ESHET_SERVER environment variable,
-/// which should either contain just a host name (for port 11236) , or a host
+/// which should either contain just a host name (for port 11236), or a host
 /// name and port number separated by a colon
 std::pair<std::string, int> get_host_port();
