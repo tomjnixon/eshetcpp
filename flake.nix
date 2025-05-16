235e74a4b49cc9138d26ee9c7e2110ee0a482eeb
Thomas Nixon <tom@tomn.co.uk>
2025-05-16 11:44:34 +0100

    nix: format flake.nix

--- flake.nix
+++ flake.nix
@@ -4,14 +4,25 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
       rec {
         packages.eshetcpp = pkgs.stdenv.mkDerivation {
           name = "eshetcpp";
           src = ./.;
-          nativeBuildInputs = [ pkgs.cmake pkgs.ninja ];
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.ninja
+          ];
           checkPhase = ''
             ./test/test_msgpack
             ./src/eshet --help
